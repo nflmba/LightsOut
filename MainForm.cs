a64eb1b266dd223683ab4102c4f4d35a1db67a4e
nflmba <56656948+nflmba@users.noreply.github.com>
2020-09-14 19:54:38 -0500

    sperated the GUI and game logic code

--- MainForm.cs
+++ MainForm.cs
@@ -16,19 +16,13 @@ namespace LightsOut
         private const int GridLength = 200; // Size in pixels of grid
         private const int NumCells = 3; // Number of cells in grid
         private const int CellLength = GridLength / NumCells;
-        private bool[,] grid; // Stores on/off state of cells in grid
-        private Random rand; // Used to generate random numbers
+        private LightsOutGame lightsOutGame;
 
         public MainForm()
         {
             InitializeComponent();
 
-            rand = new Random();
-            grid = new bool[NumCells, NumCells];
-            // Turn entire grid on
-            for (int r = 0; r < NumCells; r++)
-                for (int c = 0; c < NumCells; c++)
-                    grid[r, c] = true;
+            lightsOutGame = new LightsOutGame();
         }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -47,7 +41,7 @@ namespace LightsOut
                     // grid section
                     Brush brush;
                     Pen pen;
-                    if (grid[r, c])
+                    if (lightsOutGame.GetGridValue(r, c))
                     {
                         pen = Pens.Black;
                         brush = Brushes.White; // On
@@ -77,14 +71,11 @@ namespace LightsOut
             int r = (e.Y - GridOffset) / CellLength;
             int c = (e.X - GridOffset) / CellLength;
             // Invert selected box and all surrounding boxes
-            for (int i = r - 1; i <= r + 1; i++)
-                for (int j = c - 1; j <= c + 1; j++)
-                    if (i >= 0 && i < NumCells && j >= 0 && j < NumCells)
-                        grid[i, j] = !grid[i, j];
+            lightsOutGame.Move(r, c);
             // Redraw grid
             this.Invalidate();
             // Check to see if puzzle has been solved
-            if (PlayerWon())
+            if (lightsOutGame.IsGameOver())
             {
                 // Display winner dialog box
                 MessageBox.Show(this, "Congratulations! You've won!", "Lights Out!",
@@ -92,31 +83,9 @@ namespace LightsOut
             }
         }
 
-        private bool PlayerWon()
-        {
-            bool result = true;
-
-            for (int r = 0; r < NumCells; r++)
-            {
-                for (int c = 0; c < NumCells; c++)
-                {
-                    if (grid[r, c] == true)
-                    {
-                        result = false;
-                    }
-                }
-            }
-
-            return result;
-        }
-
         private void newGameButton_Click(object sender, EventArgs e)
         {
-            // Fill grid with either white or black
-            for (int r = 0; r < NumCells; r++)
-                for (int c = 0; c < NumCells; c++)
-                    grid[r, c] = rand.Next(2) == 1;
-            // Redraw grid
+            lightsOutGame.NewGame();
             this.Invalidate();
         }
 
@@ -139,6 +108,103 @@ namespace LightsOut
         private void exitButton_Click(object sender, EventArgs e)
         {
             Close();
+        }
+    }
+
+    class LightsOutGame
+    {
+        private int gridSize = 3;               // Number of cells in grid
+        private bool[,] grid;                   // Stores on/off state of cells in grid
+        private Random rand;		        // Used to generate random numbers
+
+        public const int MaxGridSize = 7;
+        public const int MinGridSize = 3;
+
+        // Returns the number of horizontal/vertical cells in the grid
+        public int GridSize
+        {
+            get
+            {
+                return gridSize;
+            }
+            set
+            {
+                if (value >= MinGridSize && value <= MaxGridSize)
+                {
+                    gridSize = value;
+                    grid = new bool[gridSize, gridSize];
+                    NewGame();
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("NumCells must be between " +
+                        MinGridSize + " and " + MaxGridSize + ".");
+                }
+            }
+        }
+
+        public LightsOutGame()
+        {
+            rand = new Random();    // Initialize random number generator
+
+            GridSize = MinGridSize;
+        }
+
+        // Returns the grid value at the given row and column
+        public bool GetGridValue(int row, int col)
+        {
+            return grid[row, col];
+        }
+
+        // Randomizes the grid
+        public void NewGame()
+        {
+            // Fill grid with either white or black
+            for (int r = 0; r < gridSize; r++)
+            {
+                for (int c = 0; c < gridSize; c++)
+                {
+                    grid[r, c] = rand.Next(2) == 1;
+                }
+            }
+        }
+
+        // Inverts the selected box and all surrounding boxes
+        public void Move(int row, int col)
+        {
+            if (row < 0 || row >= gridSize || col < 0 || col >= gridSize)
+            {
+                throw new ArgumentException("Row or column is outside the legal range of 0 to "
+                    + (gridSize - 1));
+            }
+
+            for (int i = row - 1; i <= row + 1; i++)
+            {
+                for (int j = col - 1; j <= col + 1; j++)
+                {
+                    if (i >= 0 && i < gridSize && j >= 0 && j < gridSize)
+                    {
+                        grid[i, j] = !grid[i, j];
+                    }
+                }
+            }
+        }
+
+        // Returns true if all cells are off
+        public bool IsGameOver()
+        {
+            for (int r = 0; r < gridSize; r++)
+            {
+                for (int c = 0; c < gridSize; c++)
+                {
+                    if (grid[r, c])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
